1c92c27bee772961494fed930179e4bb747a4262
Igor <sshutdownow@users.noreply.github.com>
2017-07-27 12:25:16 +0200

    Add files via upload

---
+++ pg_sendmail.sql
@@ -0,0 +1,34 @@
+
+-- complain if script is sourced in psql, rather than via CREATE EXTENSION
+\echo Use "CREATE EXTENSION pg_sendmail" to load this file. \quit
+
+
+CREATE OR REPLACE FUNCTION mail(mailfrom text, rcptto text, subject text, msg_body text, headers text)
+    RETURNS boolean AS
+    '$libdir/pg_sendmail', 'mail'
+    LANGUAGE C VOLATILE STRICT;
+
+CREATE OR REPLACE FUNCTION sendmail(
+    text,
+    text,
+    text,
+    text)
+  RETURNS boolean AS
+  $BODY$declare
+  mailfrom alias for $1;
+  rcptto alias for $2;
+  subject alias for $3;
+  msg_body alias for $4;
+  result boolean;
+  begin
+       result = (select mail(  mailfrom,
+                            rcptto,
+                            convert_from(convert_to(subject, 'utf-8'), 'latin-1')::text,
+                            convert_from(convert_to(msg_body, 'utf-8'), 'latin-1')::text,
+                            E'MIME-Version: 1.0\nContent-Type: text/plain; charset=\"utf-8\"\nContent-Transfer-Encoding: 8bit'));
+	return result;
+  end;
+  $BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+  ALTER FUNCTION sendmail(text, text, text, text) OWNER TO postgres;
